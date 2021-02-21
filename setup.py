8f8c6be6eefa89f852d5a9fe6dd3b0aecf804da4
Denis <pda.prostor@gmail.com>
2021-02-22 01:54:24 +0300

    less_1: create setup.py for installable package        - fix

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-# from os.path import join, dirname
+from os.path import join, dirname
 
 
 setup(
-    name='My Framework on Python(WSGI)',
+    name='MFP',
     version='less_1',
     packages=find_packages(),
-    # long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
     install_requires=[
         'gunicorn==20.0.4',
         'Jinja2==2.11.3',
