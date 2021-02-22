f4d191ab2df341bcb2b163ff537afed6e79a05e6
Denis <pda.prostor@gmail.com>
2021-02-22 15:38:22 +0300

    fix

--- MFP/core.py
+++ MFP/core.py
@@ -1,55 +1,3 @@
-# PC. отдельные контроллеры на каждую страницу
-def index_view(request):
-    return '200', [b'MAIN']
-
-
-def products_view(request):
-    return '200', [b'products view']
-
-
-def about_view(request):
-    return '200', [b'about']
-
-
-def contacts_view(request):
-    return '200', [b'contact']
-
-
-def page_404_view(request):
-    return '404', [b'page_404']
-
-
-# def _view(request):
-#     return '00',[b'']
-
-# FC. общие контроллеры на все страницы
-def fc_secret_key(request):
-    # добавляем данные в запрос
-    request['secret_key'] = '123456'
-
-
-def fc_login(request):
-    request['login'] = 'user'
-
-
-def fc_password(request):
-    request['password'] = 'password'
-
-
-def fc_is_admin(request):
-    request['is_admin'] = False
-
-
-fronts_controllers = [fc_secret_key, fc_login, fc_password, fc_is_admin]
-
-# Routes. соответствие адресу свой контроллер
-routes = {
-    '/': index_view,
-    '/products/': products_view,
-    '/about/': about_view,
-    '/contacts/': contacts_view
-}
-
 
 class Application:
     """
@@ -75,7 +23,9 @@ class Application:
         if path in self.urls_view:
             page_controller = self.urls_view[path]
         else:
-            page_controller = page_404_view
+            # если страницы нет, то отдаём код 404 и возвращаем ответ в байтах
+            start_response('404', [('Content-Type', 'text/html')])
+            return b'<h1>Page not found ! ! !</h1>'
         request = {}
         # применяем FC
         for fc in self.fronts:
@@ -85,7 +35,4 @@ class Application:
         # передаём код и заголовки
         start_response(code, [('Content-Type', 'text/html')])
         # возвращаем сам ответ в виде списка из bite
-        return [answer]
-
-
-application = Application(routes, fronts_controllers)
+        return [answer.encode('utf-8')]
