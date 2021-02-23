06f4557ee6f4d180df2f12b0c3830ea417286bc8
Denis <pda.prostor@gmail.com>
2021-02-24 00:18:37 +0300

    fix - рендер наследованныз шаблонов

--- MFP/templator.py
+++ MFP/templator.py
@@ -1,10 +1,13 @@
 """ Шаблонизатор на основе jinja2 """
 from os import path
-
 from jinja2 import Template
 
+# для рендера наследованных шаблонов
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
-def render(template_file, temp_folder='templates', **kwargs):
+
+def render(template_file, temp_folder='templates/', **kwargs):
     """
     Рендер шаблона с указанными данными.
     :param template_file: файл шаблона
@@ -12,9 +15,17 @@ def render(template_file, temp_folder='templates', **kwargs):
     :param kwargs: данные
     :return: отрендеренный шаблон
     """
+    # создаём экземпляр загрузчика шаблонов
+    env = Environment()
+    # указываем место поиска шаблонов
+    env.loader = FileSystemLoader(temp_folder)
+    # отдаём загрузчику требуемый шаблон
+    tmpl = env.get_template(template_file)
+
     # открываем и читаем файл
-    with open(path.join(temp_folder, template_file), encoding='utf-8') as f:
-        template = Template(f.read())
+    # with open(path.join(temp_folder, template_file), encoding='utf-8') as f:
+    #     template = Template(f.read())
+
     # рендерим с параметрами
-    ready_template = template.render(**kwargs)
+    ready_template = tmpl.render(**kwargs)
     return ready_template
